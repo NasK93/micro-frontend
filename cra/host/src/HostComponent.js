f00afb1ac9ebd548778326988da2d427bfc3e10d
NikotoKoto <floris.nicolas77@gmail.com>
2024-06-12 16:35:07 +0200

    Test API

---
+++ cra/host/src/HostComponent.js
@@ -0,0 +1,30 @@
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const HostComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default HostComponent;
